9ce63bf817455557fec4cb5a0ee290b68fd250c2
neoMichael <neomcl@gmail.com>
2020-04-18 23:49:15 +0100

    Tab navigation working

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,47 +1,10 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
-
+ 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
-  {
-    path: 'home',
-    loadChildren: () => import('./pages/home/home.module').then( m => m.HomePageModule)
-  },
-  {
-    path: 'tabs',
-    loadChildren: () => import('./pages/tabs/tabs.module').then( m => m.TabsPageModule)
-  },
-  {
-    path: 'characters',
-    loadChildren: () => import('./pages/characters/characters.module').then( m => m.CharactersPageModule)
-  },
-  {
-    path: 'characterdetails',
-    loadChildren: () => import('./pages/characterdetails/characterdetails.module').then( m => m.CharacterdetailsPageModule)
-  },
-  {
-    path: 'episodes',
-    loadChildren: () => import('./pages/episodes/episodes.module').then( m => m.EpisodesPageModule)
-  },
-  {
-    path: 'episodedetails',
-    loadChildren: () => import('./pages/episodedetails/episodedetails.module').then( m => m.EpisodedetailsPageModule)
-  },
-  {
-    path: 'quotes',
-    loadChildren: () => import('./pages/quotes/quotes.module').then( m => m.QuotesPageModule)
-  },
-  {
-    path: 'quotedetails',
-    loadChildren: () => import('./pages/quotedetails/quotedetails.module').then( m => m.QuotedetailsPageModule)
-  },
-  {
-    path: 'deathcount',
-    loadChildren: () => import('./pages/deathcount/deathcount.module').then( m => m.DeathcountPageModule)
-  },
+  { path: '', loadChildren: () => import('./pages/tabs/tabs.module').then( m => m.TabsPageModule)}
 ];
-
+ 
 @NgModule({
   imports: [
     RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
--- src/app/pages/tabs/tabs-routing.module.ts
+++ src/app/pages/tabs/tabs-routing.module.ts
@@ -1,15 +1,79 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
+ 
 import { TabsPage } from './tabs.page';
-
+ 
 const routes: Routes = [
   {
+    path: 'tabs',
+    component: TabsPage,
+    children: [
+      {
+        path: 'home',
+        children: [
+          {
+            path: '',
+            loadChildren: () => import('../home/home.module').then( m => m.HomePageModule)
+          }
+        ]
+      },
+      {
+        path: 'characters',
+        children: [
+          {
+            path: '',
+            loadChildren: () => import('../characters/characters.module').then( m => m.CharactersPageModule)
+          },
+          {
+            path: ':id',
+            loadChildren: () => import('../characterdetails/characterdetails.module').then( m => m.CharacterdetailsPageModule)
+          }
+        ]
+      },
+      {
+        path: 'episodes',
+        children: [
+          {
+            path: '',
+            loadChildren: () => import('../episodes/episodes.module').then( m => m.EpisodesPageModule)
+          },
+          {
+            path: ':id',
+            loadChildren: () => import('../episodedetails/episodedetails.module').then( m => m.EpisodedetailsPageModule)
+          }
+        ]
+      },
+      {
+        path: 'quotes',
+        children: [
+          {
+            path: '',
+            loadChildren: () => import('../quotes/quotes.module').then( m => m.QuotesPageModule)
+          },
+          {
+            path: ':id',
+            loadChildren: () => import('../quotedetails/quotedetails.module').then( m => m.QuotedetailsPageModule)
+          }
+        ]
+      },
+      {
+        path: 'deathcount',
+        children: [
+          {
+            path: '',
+            loadChildren: () => import('../deathcount/deathcount.module').then( m => m.DeathcountPageModule)
+          }
+        ]
+      }
+    ]
+  },
+  {
     path: '',
-    component: TabsPage
+    redirectTo: '/tabs/home',
+    pathMatch: 'full'
   }
 ];
-
+ 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule],
