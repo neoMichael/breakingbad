80bc0fce3d46adbf8c5ee5f71539854f7297758f
neoMichael <neomcl@gmail.com>
2020-04-19 02:07:04 +0100

    Api

--- src/app/pages/characterdetails/characterdetails.page.ts
+++ src/app/pages/characterdetails/characterdetails.page.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ActivatedRoute } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+ 
 @Component({
   selector: 'app-characterdetails',
   templateUrl: './characterdetails.page.html',
   styleUrls: ['./characterdetails.page.scss'],
 })
 export class CharacterdetailsPage implements OnInit {
-
-  constructor() { }
-
+ 
+  character: any;
+ 
+  constructor(private activatedRoute: ActivatedRoute, private http: HttpClient) { }
+ 
   ngOnInit() {
+    let id = this.activatedRoute.snapshot.paramMap.get('char_id');
+    this.http.get(`https://www.breakingbadapi.com/api/characters/${id}`).subscribe(res => {
+      this.character = res;
+    });
   }
-
+ 
 }
--- src/app/pages/characters/characters.page.ts
+++ src/app/pages/characters/characters.page.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+ 
 @Component({
   selector: 'app-characters',
   templateUrl: './characters.page.html',
   styleUrls: ['./characters.page.scss'],
 })
 export class CharactersPage implements OnInit {
-
-  constructor() { }
-
+ 
+  characters: Observable<any>;
+ 
+  constructor(private router: Router, private http: HttpClient) { }
+ 
   ngOnInit() {
+    this.characters = this.http.get('https://www.breakingbadapi.com/api/characters/');
   }
-
+ 
+  openDetails(character) {
+    let split = character.url.split('/');
+    let characterId = split[split.length-2];
+    this.router.navigateByUrl(`/tabs/characters/${characterId}`);
+  }
 }
